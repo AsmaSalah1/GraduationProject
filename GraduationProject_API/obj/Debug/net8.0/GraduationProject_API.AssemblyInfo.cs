b1174fd155d384c8900c4139bac1f63dc3735995
HaGGaG128 <ahmed.haggag@businessstrugglessolutions.com>
2025-03-20 10:56:26 +0200

    Updated .gitignore to exclude unnecessary files

--- GraduationProject_API/obj/Debug/net8.0/GraduationProject_API.AssemblyInfo.cs
+++ GraduationProject_API/obj/Debug/net8.0/GraduationProject_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GraduationProject_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36cc56c91b15ca864aae901caecbadb519e4b9d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+490d5e8b4483bbdb3f2e52a6dd50619a7983df1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GraduationProject_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GraduationProject_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
